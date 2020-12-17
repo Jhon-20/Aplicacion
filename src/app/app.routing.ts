701d5f44a35c07d0aca0b76fb98b476a4989c9ce
Jhon Soplopuco <67161649+Jhon-20@users.noreply.github.com>
2020-12-17 10:57:38 -0700

    proyecto

---
+++ src/app/app.routing.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { DocumentacionComponent } from './pages/documentacion/documentacion.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NuevoComponent } from './pages/nuevo/nuevo.component';
+import { AngularFireAuthGuard, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+
+const redirectToLogin = ()=> redirectUnauthorizedTo(['/login']);
+
+const routes: Routes = [
+    { path: 'inicio', component:  InicioComponent},
+    { path: 'documentacion', component: DocumentacionComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'nuevo/:tarea', component: NuevoComponent,  canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectToLogin}},
+    { path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
